356484c047fdd4082d2da671f1063fa9b862714e
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2024-10-11 00:43:54 +0000

    Add these files

---
+++ frontend/src/components/Dashboard1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard1 = () => {
+    return (
+        <div>
+            <p>This is Dashboard 1.</p>
+        </div>
+    )
+}
+
+export default Dashboard1;
---
+++ frontend/src/components/Dashboard2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard2 = () => {
+    return (
+        <div>
+            <p>This is Dashboard 2.</p>
+        </div>
+    )
+}
+
+export default Dashboard2;
